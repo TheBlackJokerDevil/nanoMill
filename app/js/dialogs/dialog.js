8d161cb57ff0d13b7c902156b0c76a9e82ccfd8e
Apfelclonk <fridjofmilke@googlemail.com>
2017-04-11 23:34:31 +0200

    added optional dialog head to dialog

--- app/js/dialogs/dialog.js
+++ app/js/dialogs/dialog.js
@@ -5,11 +5,18 @@ class Dialog {
 		this.modal = document.createElement("div")
 		this.modal.className = "dlg-modal"
 		
-		this.modal.innerHTML = `<div class="dlg-container flex-col"><div class="dlg-body"></div><div class="flex-fill"></div><div class="dlg-footer"></div></div>`
+		this.modal.innerHTML =
+			`<div class="dlg-container flex-col">
+				<div class="dlg-head"></div>
+				<div class="dlg-body"></div>
+				<div class="flex-fill"></div>
+				<div class="dlg-footer"></div>
+			</div>`
 		
 		let con = this.modal.firstChild
-		this.body = con.firstChild
-		this.footer = con.lastChild
+		this.head = con.firstElementChild
+		this.body = this.head.nextElementSibling
+		this.footer = con.lastElementChild
 		
 		con.style.width = width + "px"
 		con.style.height = height + "px"

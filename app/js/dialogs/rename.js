2fcf7e4878115d39d9e682378a5c10dab43b5f26
Apfelclonk <fridjofmilke@googlemail.com>
2017-04-12 14:17:22 +0200

    added option to specify rename dialog description to be reused in other contexts than renaming files

--- app/js/dialogs/rename.js
+++ app/js/dialogs/rename.js
@@ -1,11 +1,14 @@
 let Wizard = require(path.join(__rootdir, "js", "dialogs", "wizard.js"))
 let Form = require("./../lib/form.js")
 
-module.exports = function(oldName, cb) {
+module.exports = function(oldName, cb, desc) {
+	if(!desc)
+		desc = "Enter new name"
+	
 	let wzd = new Wizard(300, 150, [
 		new Form([{
 				type: "desc",
-				text: "Enter new filename"
+				text: desc
 			}, {
 				type: "shorttext",
 				align: "center",

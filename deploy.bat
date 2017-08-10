82b469d7caea618d80b28933b956c457ac78fb28
Apfelclonk <fridjofmilke@googlemail.com>
2017-08-10 21:39:49 +0200

    updated electron version in deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 call npm
 call npm install ./app --global --save
-call electron-packager ./app nanoMill --all --asar --electron-version=1.6.2 --overwrite --out=./build/ --icon=./app/icon/icon
+call electron-packager ./app nanoMill --all --asar --electron-version=1.7.5 --overwrite --out=./build/ --icon=./app/icon/icon
 pause
